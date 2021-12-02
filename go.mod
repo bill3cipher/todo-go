abf725f9f24617ecd63b1659a6752c09187c5db1
(っ◔◡◔)っ ɴᴜᴇ <chakhritz@gmail.com>
2021-12-02 23:04:54 +0700

    ldflags
    ldflags
    liveness probe
    rate limit

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/chakhrits/todo-api
 go 1.17
 
 require (
+	github.com/c2h5oh/datasize v0.0.0-20200825124411-48ed595a09d2 // indirect
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/gin-gonic/gin v1.7.7 // indirect
@@ -11,19 +12,26 @@ require (
 	github.com/go-playground/validator/v10 v10.9.0 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/influxdata/tdigest v0.0.1 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.3 // indirect
 	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mattn/go-sqlite3 v1.14.9 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/tsenart/go-tsz v0.0.0-20180814235614-0bd30b3df1c3 // indirect
+	github.com/tsenart/vegeta v12.7.0+incompatible // indirect
 	github.com/ugorji/go/codec v1.2.6 // indirect
 	golang.org/x/crypto v0.0.0-20211117183948-ae814b36b871 // indirect
+	golang.org/x/net v0.0.0-20211201190559-0a0e4e1bb54c // indirect
 	golang.org/x/sys v0.0.0-20211124211545-fe61309f8881 // indirect
 	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/time v0.0.0-20211116232009-f0f3c7e86c11
 	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gorm.io/driver/sqlite v1.2.6 // indirect
--- main.go
+++ main.go
@@ -14,13 +14,26 @@ import (
 	"github.com/chakhrits/todo-api/todo"
 	"github.com/gin-gonic/gin"
 	"github.com/joho/godotenv"
+	"golang.org/x/time/rate"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
 )
 
+var (
+	buildcommit = "dev"
+	buildtime   = time.Now().String()
+)
+
 func main() {
+	//Liveness probe check api
+	_, err := os.Create("/tmp/live")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer os.Remove("/tmp/live")
+
 	//get environment from env
-	err := godotenv.Load("local.env")
+	err = godotenv.Load("local.env")
 	if err != nil {
 		log.Printf("please consider environment variable: %s", err)
 	}
@@ -34,19 +47,31 @@ func main() {
 	db.AutoMigrate(&todo.Todo{})
 
 	r := gin.Default() //Create default router
+	//===============Router===============
+	r.GET("/health", func(c *gin.Context) {
+		c.Status(http.StatusOK)
+	})
+	r.GET("limit", limitedHandler)
+	r.GET("/x", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"buildcommit": buildcommit,
+			"buildtime":   buildtime,
+		})
+	})
+
 	//handler with path
 	r.GET("/ping", pingpongHandler)
 	handler := todo.NewTodoHandler(db)
 
-	r.GET("/tokenz", auth.AccessToken(os.Getenv("SIGN")))
+	r.GET("/token", auth.AccessToken(os.Getenv("SIGN")))
 	//สร้างgroupเพื่อแยกว่าpathไหนprotectได้บ้าง
 	protected := r.Group("", auth.Protect([]byte(os.Getenv("SIGN"))))
 	protected.POST("/todos", handler.NewTask)
+	//===============End Router===============
 
 	//Graceful shutdown
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
-
 	s := &http.Server{
 		Addr:           ":" + os.Getenv("PORT"),
 		Handler:        r,
@@ -54,13 +79,11 @@ func main() {
 		WriteTimeout:   10 * time.Second,
 		MaxHeaderBytes: 1 << 20,
 	}
-
 	go func() {
 		if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
 			log.Fatalf("listen: %s\n", err)
 		}
 	}()
-
 	<-ctx.Done()
 	stop()
 	fmt.Println("shutting down graceful, press CTRL+C again to force")
@@ -70,6 +93,18 @@ func main() {
 
 func pingpongHandler(c *gin.Context) {
 	c.JSON(200, gin.H{
+		"message": "pong",
+	})
+}
+
+var limiter = rate.NewLimiter(5, 5)
+
+func limitedHandler(c *gin.Context) {
+	if limiter.Allow() {
+		c.AbortWithStatus(http.StatusTooManyRequests)
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
 		"message": "pong",
 	})
 }
